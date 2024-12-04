0b348c2b76a6eaf33ffc6a66f9ea5dc0d0a6cd65
bmansour1 <117964164+bmansour1@users.noreply.github.com>
2024-12-04 16:47:04 -0500

    added routing and google api

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,23 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { ClerkProvider } from '@clerk/clerk-react'
-
+// main.tsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
+import { ClerkProvider } from '@clerk/clerk-react';
+import { BrowserRouter } from 'react-router-dom';
 
 // Import your publishable key from the environment
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
 
 // Check for the publishable key
 if (!PUBLISHABLE_KEY) {
-  throw new Error("Missing Publishable Key")
+  throw new Error('Missing Publishable Key');
 }
 
-// Render the root of your app with ClerkProvider
+// Render the root of your app with ClerkProvider and BrowserRouter
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </ClerkProvider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
