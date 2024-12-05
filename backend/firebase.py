254f692737aeb5252abae4d3f6f5174b08512a55
Jonathan Wang <cobaltsleet49@gmail.com>
2024-12-05 01:34:08 -0500

    changed backend endpoint to match frontend format

--- backend/firebase.py
+++ backend/firebase.py
@@ -1,5 +1,6 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+from google.cloud.firestore import DELETE_FIELD
 
 # Use a service account
 cred = credentials.Certificate('key.json') # Assuming you stored your key file as 'key.json' under backend/
@@ -43,3 +44,26 @@ def delete_user_chat(user_id):
     chat_ref.delete()
     return f"Chats for user with ID {user_id} has been successfully deleted"
 
+# User Diagnoses
+def get_user_diagnosis_list(user_id):
+    diagnosis_ref = db.collection('Diagnoses').document(user_id)
+    diagnosis_dict = diagnosis_ref.get().to_dict()
+    diagnosis_list = [{"timestamp": k, **v} for k, v in diagnosis_dict.items()] # Extract dateTime and add it as a parameter; convert from a dict to a list
+    return diagnosis_list
+
+def add_user_diagnosis(user_id, new_diagnosis):
+    diagnosis_ref = db.collection('Diagnoses').document(user_id)
+    diagnosis_ref.set(new_diagnosis, merge=True)
+    return f"New diagnosis for user with ID {user_id} has been successfully added"
+
+def delete_user_diagnosis(user_id, date_time):
+    diagnosis_ref = db.collection('Diagnoses').document(user_id)
+    diagnosis_ref.update({
+        date_time: DELETE_FIELD
+    })
+    return f"Diagnosis for user with ID {user_id} has been successfully deleted"
+
+def delete_user_diagnosis_list(user_id):
+    diagnosis_ref = db.collection('Diagnoses').document(user_id)
+    diagnosis_ref.delete()
+    return f"All diagnoses for user with ID {user_id} have been successfully deleted"
